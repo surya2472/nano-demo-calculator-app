2788ca08fc6268d9da6d8fe9a065c32c64d99d8c
surya2472 <124003393@sastra.ac.in>
2023-08-09 10:50:11 +0530

    solution

--- server.py
+++ server.py
@@ -1,20 +1,51 @@
-from flask import Flask
+from flask import Flask,request,jsonify
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    pass
-    return ''
+    return 'Hello world!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    try:
+        # Assuming the request data is sent as JSON containing 'num1' and 'num2'
+        data = request.json
+        num1 = data.get('first')
+        num2 = data.get('second')
+
+        if num1 is None or num2 is None:
+            return "Invalid input: 'num1' and 'num2' must be provided as JSON data.", 400
+
+        result = num1 + num2
+
+        return {
+            'result': result
+        }, 200
+
+    except Exception as e:
+        return str(e), 500
+    
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    try:
+        # Assuming the request data is sent as JSON containing 'num1' and 'num2'
+        data = request.json
+        num1 = data.get('first')
+        num2 = data.get('second')
+
+        if num1 is None or num2 is None:
+            return jsonify({"error": "Invalid input: 'num1' and 'num2' must be provided as JSON data."}), 400
+
+        result = num1 - num2
+
+        return jsonify({"result": result}), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
